579029d3c167986769a9099e689b8254a9ace0ae
Valro666 <valerian266@gmail.com>
2014-11-26 18:52:27 +0100

    modification gestion des comptes

--- Compte.cpp
+++ Compte.cpp
@@ -1,12 +1,31 @@
 #include "Compte.h"
 using namespace std;
-
 Compte::Compte(){};
 
-Compte::Compte(string id , string pw){
+Compte::Compte(string id , string pw,int type){
 	identifiant = id ;
 	password = pw ;
+
+	switch(type){
+	case COMPTE_ETUDIANT : status = COMPTE_ETUDIANT; break;	
+
+	case COMPTE_ENSEIGNANT : status = COMPTE_ENSEIGNANT; break;
+
+	case COMPTE_ADMIN : status = COMPTE_ADMIN; break;
+
+	default : status = 42; break;
+
+	}
+
+
+
 };
+
+int Compte::getStatus() const{
+	return status;
+
+}
+
 
 string Compte::getIdentifiant() const{
 	return identifiant;
@@ -18,6 +37,11 @@ string Compte::getPassword() const{
 
 string Compte::getTampon() const{
 	return tampon ;
+}
+
+void Compte::setStatus(int i){
+	status = i ;
+
 }
 
 void Compte::setTampon(string s){
--- Compte.h
+++ Compte.h
@@ -3,14 +3,17 @@
 
 #include <iostream>
 #include <string>
+#include "Constante.h"
 
 class Compte{
       public:
 	          Compte();
-	          Compte(std::string id , std::string pw);
+	          Compte(std::string id , std::string pw,int type);
 	          std::string getIdentifiant() const;
               std::string getPassword() const;
               std::string getTampon() const;
+			  int getStatus() const;
+			  void setStatus(int i);
               void setTampon(std::string s);
               void setIdentifiant(std::string s);
               void setPassword(std::string s);
@@ -18,5 +21,6 @@ class Compte{
               std::string identifiant ;
 	          std::string password;
               std::string tampon;
+			  int status;
 };
 #endif
