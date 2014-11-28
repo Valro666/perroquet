85c094c5e3b035ee7c6b39624892a9425bd87164
unknown <aissa2u@fst-ats-320-w11.ad.univ-lorraine.fr>
2014-11-28 10:56:02 +0100

    Depot.h done

---
+++ Depot.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "Constante.h"
+
+class Depot{
+      public:
+	          Depot();
+	          Depot(std::string intitule , std::string chemin, std::bool ouvert);
+	          std::string getIntitule() const;
+	          void setIntitule(std::string inti) const;
+              std::string getChemin() const;
+              void setChemin(std::string road) const;
+              std::bool getOuvert() const;
+              void setOuvert() const;
+			  
+              
+      protected :
+              std::string intitule ;
+	          std::string chemin;
+              std::bool chemin;
+};
+#endif
