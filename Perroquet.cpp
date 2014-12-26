8256ae0d41c9913baa8ec0ce7f95d666c3c2cacc
unknown <franck_audiger@hotmail.com>
2014-12-26 02:13:08 +0100

    ajout de la vue inscription avec la transition depuis la connexion, il faudra probablement rafraichir les autres vues quand elle passe de caché à affiché car elle seront toute crée au debut de l'appli avec l evenement de type WM_SHOWWINDOW probablement

--- Perroquet.cpp
+++ Perroquet.cpp
@@ -5,28 +5,47 @@
 #include "Perroquet.h"
 
 //declaration des variables
+int modeDAffichageFenetre;
+
 HWND fenetreConnexion;
 HWND fenetreConnexion_EditTextLogin;
 HWND fenetreConnexion_EditTextMdp;
+
+HWND fenetreInscription;
+HWND fenetreInscription_EditTextLogin;
+HWND fenetreInscription_EditTextMdp;
+HWND fenetreInscription_EditTextMdpConfirm;
+
 HINSTANCE instance;
 
 //declaration des fonctions de création des fenetres
 HWND getFenetreConnexion();
+HWND getFenetreInscription();
+
 //declaration des procedures de gestion des fenetres
 LRESULT CALLBACK procedureFenetreConnexion(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK procedureFenetreInscription(HWND, UINT, WPARAM, LPARAM);
+
 //declaration des fonctions de construction des vues
 VOID buildFenetreConnexion(HWND fenetrePrincipale);
+VOID buildFenetreInscription(HWND fenetrePrincipale);
+
 //declaration des fonctionsde commande
 VOID commandFenetreConnexion(HWND fenetrePrincipale, WPARAM wParam);
+VOID commandFenetreInscription(HWND fenetrePrincipale, WPARAM wParam);
 
 int APIENTRY WinMain(HINSTANCE cetteInstance, HINSTANCE precedenteInstance,
 LPSTR lignesDeCommande, int modeDAffichage)
 {
+	modeDAffichageFenetre = modeDAffichage;
 	instance = cetteInstance;
     MSG message;
     fenetreConnexion = getFenetreConnexion();
+	fenetreInscription = getFenetreInscription();
 	if (!fenetreConnexion) return FALSE;
-    ShowWindow(fenetreConnexion, modeDAffichage);
+	//mettre ici dans le showwindows la fenetre que le veut tester sinon mettre fenetreConnexion pour un comportement normal
+    ShowWindow(fenetreConnexion, modeDAffichageFenetre);
+	ShowWindow(fenetreInscription, SW_HIDE);
     UpdateWindow(fenetreConnexion);
 
 
@@ -63,14 +82,38 @@ HWND getFenetreConnexion(){
 	}
 	return rep;
 }
+HWND getFenetreInscription(){
+	HWND rep;
+	WNDCLASS classeFenetre;
 
+    classeFenetre.style = 0;
+    classeFenetre.lpfnWndProc = procedureFenetreInscription;
+    classeFenetre.cbClsExtra = 0;
+    classeFenetre.cbWndExtra = 0;
+    classeFenetre.hInstance = NULL;
+    classeFenetre.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    classeFenetre.hCursor = LoadCursor(NULL, IDC_ARROW);
+    classeFenetre.hbrBackground = (HBRUSH)(1 + COLOR_BTNFACE);
+    classeFenetre.lpszMenuName =  TEXT("ID_MENU_INSCRIPTION");
+    classeFenetre.lpszClassName = TEXT("classeFInscription");
 
+    // On prévoit quand même le cas où ça échoue
+    if(RegisterClass(&classeFenetre)){
+    rep = CreateWindow(TEXT("classeFInscription"), TEXT("Perroquet - Inscription"), WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+                                   CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
+                                                   NULL, NULL, instance, NULL);
+	}
+	return rep;
+}
 //procedure des fenetre :
 
 LRESULT CALLBACK procedureFenetreConnexion(HWND fenetre, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+		case WM_SHOWWINDOW :
+			//on rafraichie la vue si besoin
+			return 0;
         case WM_CREATE:
 			buildFenetreConnexion(fenetre);
             return 0;
@@ -79,6 +122,28 @@ LRESULT CALLBACK procedureFenetreConnexion(HWND fenetre, UINT message, WPARAM wP
             return 0;
 		case WM_COMMAND:
 			commandFenetreConnexion(fenetre,wParam);
+        default:
+            return DefWindowProc(fenetre, message, wParam, lParam);
+    }
+}
+LRESULT CALLBACK procedureFenetreInscription(HWND fenetre, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+		case WM_SHOWWINDOW :
+			//on rafraichie la vue si besoin
+			
+					MessageBox(fenetre, TEXT("show inscription"), TEXT("Bonjour."), MB_ICONINFORMATION);
+			return 0;
+        case WM_CREATE:
+			buildFenetreInscription(fenetre);
+					MessageBox(fenetre, TEXT("create inscription"), TEXT("Bonjour."), MB_ICONINFORMATION);
+            return 0;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+		case WM_COMMAND:
+			commandFenetreInscription(fenetre,wParam);
         default:
             return DefWindowProc(fenetre, message, wParam, lParam);
     }
@@ -139,14 +204,87 @@ VOID buildFenetreConnexion(HWND fenetrePrincipale){
         instance,
         NULL);
 }
+VOID buildFenetreInscription(HWND fenetrePrincipale){
+	HWND label1 = CreateWindow(
+		TEXT("STATIC"),
+		TEXT("Nom d'utilisateur :"),
+		WS_VISIBLE | WS_CHILD | ES_RIGHT,
+		60,42,
+		120,20,
+        fenetrePrincipale,
+        NULL,
+        instance,
+        NULL);
+	fenetreInscription_EditTextLogin=CreateWindow(
+        TEXT("EDIT"),
+        TEXT(""),
+        WS_CHILD|WS_VISIBLE|ES_LEFT,
+        200,40,
+        160,20,
+        fenetrePrincipale,
+        NULL,
+        instance,
+        NULL);
 
+	HWND label2 = CreateWindow(
+		TEXT("STATIC"),
+		TEXT("Mot de passe :"),
+		WS_VISIBLE | WS_CHILD | ES_RIGHT,
+		80,72,
+		100,20,
+        fenetrePrincipale,
+        NULL,
+        instance,
+        NULL);
+	fenetreInscription_EditTextMdp=CreateWindow(
+        TEXT("EDIT"),
+        TEXT(""),
+        WS_CHILD|WS_VISIBLE|ES_LEFT|ES_PASSWORD ,
+        200,70,
+        160,20,
+        fenetrePrincipale,
+        NULL,
+        instance,
+        NULL);
+	HWND label3 = CreateWindow(
+		TEXT("STATIC"),
+		TEXT("Confirmer mot de passe :"),
+		WS_VISIBLE | WS_CHILD | ES_RIGHT,
+		15,102,
+		165,20,
+        fenetrePrincipale,
+        NULL,
+        instance,
+        NULL);
+	fenetreInscription_EditTextMdpConfirm=CreateWindow(
+        TEXT("EDIT"),
+        TEXT(""),
+        WS_CHILD|WS_VISIBLE|ES_LEFT|ES_PASSWORD ,
+        200,100,
+        160,20,
+        fenetrePrincipale,
+        NULL,
+        instance,
+        NULL);
+	HWND boutonConnexion =CreateWindow(
+		TEXT("BUTTON"),
+        TEXT("Inscription"),
+        WS_CHILD|WS_VISIBLE ,
+        100,140,
+        160,20,
+        fenetrePrincipale,
+        (HMENU)ID_INSCRIPTION_BOUTON_INSCRIPTION,
+        instance,
+        NULL);
+}
 
 //fonctions de commande :
 VOID commandFenetreConnexion(HWND fenetrePrincipale, WPARAM wParam){
 	switch(LOWORD(wParam))
             {
                 case ID_CONNEXION_MENUITEM_INSCRIPTION:
-					MessageBox(fenetrePrincipale, TEXT("inscription"), TEXT("Bonjour."), MB_ICONINFORMATION);
+					ShowWindow(fenetreConnexion, SW_HIDE);
+					ShowWindow(fenetreInscription, modeDAffichageFenetre);
                     break;
 
                 case ID_CONNEXION_BOUTON_CONNEXION:
@@ -163,6 +301,19 @@ VOID commandFenetreConnexion(HWND fenetrePrincipale, WPARAM wParam){
 					string text = "login : "+(string)login+"\nmdp : "+(string)mdp;
 					//TODO coder la connexion et rajouter le model
 					MessageBox(fenetrePrincipale, text.c_str(), TEXT("Bonjour."), MB_ICONINFORMATION);
+                    break;
+            }
+}
+
+VOID commandFenetreInscription(HWND fenetrePrincipale, WPARAM wParam){
+	switch(LOWORD(wParam))
+            {
+                case ID_INSCRIPTION_BOUTON_INSCRIPTION :
+					MessageBox(fenetrePrincipale, TEXT("inscription"), TEXT("Bonjour."), MB_ICONINFORMATION);
+                    break;
+				case ID_INSCRIPTION_MENUITEM_RETOUR :
+					ShowWindow(fenetreConnexion, modeDAffichageFenetre);
+					ShowWindow(fenetreInscription, SW_HIDE);
                     break;
             }
 }
