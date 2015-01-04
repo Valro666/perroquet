f4e0f33a1c69d02c54885b5a7a770d02cba68671
aissa04 <a.aissa89@gmail.com>
2015-01-04 22:27:21 +0100

    modification Cours.cpp

--- Cours.cpp
+++ Cours.cpp
@@ -29,46 +29,28 @@ int Cours::getLimite() const{
 	return limite;
 }
 
-Ressource Cours::getRessource(int index) {
-	std::list<Ressource>::iterator it = lRess.begin();
-	std::advance(it, index);	
-	return *it ;
+Ressource Cours::getRessource(int index) {	
+	return lRess[index] ;
 }
 
-Devoir Cours::getDevoir(int index) {
-	std::list<Devoir>::iterator it = lDev.begin();
-	std::advance(it, index);	
-	return *it ;
+Devoir Cours::getDevoir(int index) {	
+	return lDev[index] ;
 }
 
-Etudiant Cours::getEtudiant(int index) {
-	std::list<Etudiant>::iterator it = lEtu.begin();
-	std::advance(it, index);	
-	return *it ;
+Etudiant Cours::getEtudiant(int index) {	
+	return lEtu[index] ;
 }
 
 int Cours::getSizelRess() {
-	int compt = 0;
-	for (std::list<Ressource>::iterator it = lRess.begin(); it != lRess.end(); ++it){
-		compt++;
-	}
-	return compt;
+	return lRess.size();
 }
 
 int Cours::getSizelDev() {
-	int compt = 0;
-	for (std::list<Devoir>::iterator it = lDev.begin(); it != lDev.end(); ++it){
-		compt++;
-	}
-	return compt;
+	return lDev.size();
 }
 
 int Cours::getSizelEtu() {
-	int compt = 0;
-	for (std::list<Etudiant>::iterator it = lEtu.begin(); it != lEtu.end(); ++it){
-		compt++;
-	}
-	return compt;
+	return lEtu.size();
 }
 
 void Cours::setLimite(int lim){
