f883b965f4a197f891aa2d5455a8497b78404c01
aissa04 <a.aissa89@gmail.com>
2015-01-04 19:55:01 +0100

    modification Cours.h

--- Cours.h
+++ Cours.h
@@ -3,6 +3,7 @@
 #include <list> 
 #include "Enseignant.h"
 #include "Ressource.h"
+#include "Devoir.h"
 using namespace std;
 class Cours
 {
@@ -14,12 +15,16 @@ class Cours
 		int getLimite() const;
 		Enseignant getEnseignant() const;
 		Ressource getRessource (int index);
+		Devoir getDevoir (int index);
 
 		void setLimite(int lim);
 		void setEnseignant(Enseignant ens);
 		void ajouterRessource(Ressource ress);
+		void ajouterDevoir(Devoir dev);
+
 	protected :
 		Enseignant compte ;
 		int limite;
 		list<Ressource> lRess;
+		list<Devoir> lDev;
 };
