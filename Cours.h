724dff788e65b9c681ababc3cc8a60a1e68aa898
aissa04 <a.aissa89@gmail.com>
2015-01-05 02:53:54 +0100

    modification Cours.h

--- Cours.h
+++ Cours.h
@@ -10,12 +10,13 @@ class Cours
 	public:
 		Cours(void);
 		
-		Cours(Compte compte , int limite, bool acc, string inti);
+		Cours(Compte compte , int limite, bool acc, string inti, bool ouvert);
 		
 		int getLimite() const;
 		Compte getEnseignant() const;
 		string getIntitule() const;
 		bool getAccepte() ;
+		bool getOuvert();
 		Ressource getRessource (int index);
 		Devoir getDevoir (int index);
 		Compte getEtudiant (int index);
@@ -29,12 +30,14 @@ class Cours
 		void ajouterDevoir(Devoir dev);
 		void ajouterEtudiant(Compte etu);
 		void setAccepte();
+		void setOuvert();
 		void setIntitule(string inti);
 
 	protected :
 		Compte compte ;
 		int limite;
 		bool accepte;
+		bool ouvert;
 		string intitule;
 		vector<Ressource> lRess;
 		vector<Devoir> lDev;
